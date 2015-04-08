873a98410bb4234a943a29905ec2eb0f755aa49d
sukitha <sukithaj@gmail.com>
2015-04-08 09:46:39 +0530

    first commit

---
+++ config/default.js
@@ -0,0 +1,22 @@
+module.exports = {
+    Freeswitch: {
+        ip: '192.168.2.101',
+        port: 8021,
+        password: 'ClueCon',
+        httport: 8080
+    },
+
+
+
+    WebAPI: {
+
+        domain: '192.168.1.58',
+        port: 80,
+        path: '/CSReqWebApi/api/'
+    },
+
+    Redis: {
+        ip: '192.168.1.194',
+        port: 6379
+    }
+};
