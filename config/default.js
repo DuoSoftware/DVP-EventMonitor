238e8bb1cb7d529298de38137e70b406ab3a427f
sine19 <sineth17@gmail.com>
2018-06-04 11:33:32 +0530

    clear all values
    all default.js values are cleared due to security reasons.

--- config/default.js
+++ config/default.js
@@ -1,6 +1,6 @@
 module.exports = {
     "Freeswitch": {
-        ip: '159.203.160.47',
+        ip: '',
         port: 8021,
         password: 'ClueCon',
         httport: 8080
@@ -8,32 +8,32 @@ module.exports = {
 
     "DB": {
         "Type":"postgres",
-        "User":"duo",
-        "Password":"DuoS123",
+        "User":"",
+        "Password":"",
         "Port":5432,
-        "Host":"104.236.231.11",
-        "Database":"duo"
+        "Host":"",
+        "Database":""
     },
 
     Dialer: {
-        ip: '192.168.0.5',
+        ip: '',
         port: 2223
     },
 
     ARDS: {
-        ip: 'ardsliteservice.app.veery.cloud',
+        ip: '',
         port: 2225,
         version: '1.0.0.0'
     },
 
     NS: {
-        ip: 'notificationservice.app.veery.cloud',
+        ip: '',
         port: 8089,
         version: '1.0.0.0'
     },
 
     Services : {
-        interactionServiceHost: "interactions.app.veery.cloud",
+        interactionServiceHost: "",
         interactionServicePort: "3637",
         interactionServiceVersion: "1.0.0.0",
         dccaclientHost: "127.0.0.1",
@@ -43,28 +43,22 @@ module.exports = {
 
     WebAPI: {
 
-        domain: '192.168.1.58',
+        domain: '',
         port: 80,
         path: '/CSReqWebApi/api/'
     },
 
-    //Redis: {
-    //    ip: '45.55.142.207',
-    //    port: 6389,
-    //    password: "DuoS123",
-    //    db: 4
-    //},
-
+    
     "Redis":
     {
         "mode":"instance",//instance, cluster, sentinel
-        "ip": "138.197.90.92",
+        "ip": "",
         "port": 6389,
-        "user": "duo",
-        "password": "DuoS123",
+        "user": "",
+        "password": "",
         "db": 4,
         "sentinels":{
-            "hosts": "138.197.90.92,45.55.205.92,162.243.81.39",
+            "hosts": "",
             "port":16389,
             "name":"redis-cluster"
         }
@@ -72,24 +66,24 @@ module.exports = {
     },
 
     RabbitMQ: {
-        "ip":"45.55.142.207",
+        "ip":"",
         "port":"5672",
-        "user": "admin",
-        "password": "admin",
+        "user": "",
+        "password": "",
         "vhost":'/'
     },
 
     Mongo:
     {
-        "ip":"104.236.231.11",
+        "ip":"",
         "port":"27017",
-        "dbname":"dvpdb",
-        "password":"DuoS123",
-        "user":"duo",
-        "replicaset" :"104.236.231.11"
+        "dbname":"",
+        "password":"",
+        "user":"",
+        "replicaset" :""
     },
 
-    Token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdWtpdGhhIiwianRpIjoiYWEzOGRmZWYtNDFhOC00MWUyLTgwMzktOTJjZTY0YjM4ZDFmIiwic3ViIjoiNTZhOWU3NTlmYjA3MTkwN2EwMDAwMDAxMjVkOWU4MGI1YzdjNGY5ODQ2NmY5MjExNzk2ZWJmNDMiLCJleHAiOjE5MDIzODExMTgsInRlbmFudCI6LTEsImNvbXBhbnkiOi0xLCJzY29wZSI6W3sicmVzb3VyY2UiOiJhbGwiLCJhY3Rpb25zIjoiYWxsIn1dLCJpYXQiOjE0NzAzODExMTh9.Gmlu00Uj66Fzts-w6qEwNUz46XYGzE8wHUhAJOFtiRo",
+    Token: "",
     billingEnabled: false,
     evtConsumeType: 'amqp',
     UseDashboardAMQP: 'true',
